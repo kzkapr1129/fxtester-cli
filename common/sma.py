e39f84fd2bb3303e287dfa5b2565194d76342788
中山一輝 <nakayama@mackbook-air.local>
2024-12-04 09:16:26 +0900

    単純移動平均線の計算処理追加

---
+++ common/sma.py
@@ -0,0 +1,17 @@
+"""単純移動平均線計算モジュール
+"""
+from pandas import DataFrame
+
+
+def mark_sma(df: DataFrame, average=25) -> DataFrame:
+    """単純移動平均線をデータフレームに書き込む
+
+    Args:
+        df (DataFrame): ローソク足の情報が格納されたデータフレーム
+
+    Returns:
+        DataFrame: 単純移動平均線が書き込まれたデータフレーム
+    """
+    close = df['close']
+    df['sma'] = close.rolling(window=average, min_periods=average).mean()
+    return df
