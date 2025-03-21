8bf1270d4a1b57472d309c97a6f77ba46a5d3bb1
中山一輝 <nakayama@mackbook-air.local>
2025-03-21 20:44:27 +0900

    autorep8のE501の修正を戻す

--- fxtester.py
+++ fxtester.py
@@ -18,47 +18,31 @@ def main():
     """
     # 共通パーサーの初期化
     common_parser = argparse.ArgumentParser(add_help=False)
-    common_parser.add_argument(
-        "-c", "--config", default="config/config.toml", help="設定ファイルのパスを指定する")
+    common_parser.add_argument("-c", "--config", default="config/config.toml", help="設定ファイルのパスを指定する")
 
     # サブコマンドパーサーの初期化
     parser = argparse.ArgumentParser(description="", add_help=False)
-    sub_parser = parser.add_subparsers(
-        dest="mode", required=True, help="サブコマンド")
+    sub_parser = parser.add_subparsers(dest="mode", required=True, help="サブコマンド")
 
     # analyzerパーサーの初期化
-    analyzer_parser = sub_parser.add_parser(
-        "analyze", help="インジケータを計算する", parents=[common_parser])
-    analyzer_parser.add_argument("-i", "--input", type=str, required=True,
-                                 help="入力ファイルのパス (csvまたはcsvが格納されたフォルダ)")
-    analyzer_parser.add_argument("-o", "--output", type=str,
-                                 help="出力ファイルのパス")
-    analyzer_parser.add_argument(
-        "-e", "--ext", choices=["json", "csv"], help="出力ファイルの拡張子", default="json")
-    analyzer_parser.add_argument(
-        "-g", "--show-graph", action='store_true', help="検出した特徴をグラフに重畳して表示する")
-    analyzer_parser.add_argument(
-        "-s", "--sma", type=int, nargs='*', help="単純移動平均線の平均値を指定する")
-    analyzer_parser.add_argument(
-        "-k", "--ichimoku", action="store_true", help="一目均衡表を計算する")
-    analyzer_parser.add_argument(
-        "-z", "--zigzag", action="store_true", help="ジグザグを検出する")
+    analyzer_parser = sub_parser.add_parser("analyze", help="インジケータを計算する", parents=[common_parser])
+    analyzer_parser.add_argument("-i", "--input", type=str, required=True, help="入力ファイルのパス (csvまたはcsvが格納されたフォルダ)")
+    analyzer_parser.add_argument("-o", "--output", type=str, help="出力ファイルのパス")
+    analyzer_parser.add_argument("-e", "--ext", choices=["json", "csv"], help="出力ファイルの拡張子", default="json")
+    analyzer_parser.add_argument("-g", "--show-graph", action='store_true', help="検出した特徴をグラフに重畳して表示する")
+    analyzer_parser.add_argument("-s", "--sma", type=int, nargs='*', help="単純移動平均線の平均値を指定する")
+    analyzer_parser.add_argument("-k", "--ichimoku", action="store_true", help="一目均衡表を計算する")
+    analyzer_parser.add_argument("-z", "--zigzag", action="store_true", help="ジグザグを検出する")
 
     # detectorパーサーの初期化
-    detector_parser = sub_parser.add_parser(
-        "detect", help="抵抗帯の情報を検出する", parents=[common_parser])
-    detector_parser.add_argument("-i", "--input", type=str, required=True,
-                                 help="入力ファイルのパス (csvまたはcsvが格納されたフォルダ)")
-    detector_parser.add_argument("-o", "--output", type=str,
-                                 help="出力ファイルのパス")
-    detector_parser.add_argument(
-        "-e", "--ext", choices=["json", "csv"], help="出力ファイルの拡張子", default="json")
-    detector_parser.add_argument(
-        "-g", "--show-graph", action='store_true', help="検出した特徴をグラフに重畳して表示する")
-    detector_parser.add_argument(
-        "-w", "--window", type=int, help="抵抗帯判定に使用するウインドウの幅", default=1)
-    detector_parser.add_argument(
-        "-t", "--threshold", type=float, help="抵抗帯面積率の閾値", default=0.8)
+    detector_parser = sub_parser.add_parser("detect", help="抵抗帯の情報を検出する", parents=[common_parser])
+    detector_parser.add_argument("-i", "--input", type=str, required=True, help="入力ファイルのパス (csvまたはcsvが格納されたフォルダ)")
+    detector_parser.add_argument("-o", "--output", type=str, help="出力ファイルのパス")
+    detector_parser.add_argument("-e", "--ext", choices=["json", "csv"], help="出力ファイルの拡張子", default="json")
+    detector_parser.add_argument("-g", "--show-graph", action='store_true', help="検出した特徴をグラフに重畳して表示する")
+    detector_parser.add_argument("-w", "--window", type=int, help="抵抗帯判定に使用するウインドウの幅", default=1)
+    detector_parser.add_argument("-t", "--threshold", type=float, help="抵抗帯面積率の閾値", default=0.8)
+
     # コマンドのパース
     args = parser.parse_args()
 
